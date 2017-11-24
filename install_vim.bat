db2a010e39d094f53f47409c9a8b8e444333b52c
Jälv <thomas.montaigu@laposte.net>
2017-11-24 22:00:47 +0100

    fuck this

---
+++ install_vim.bat
@@ -0,0 +1,12 @@
+mkdir vimfiles
+cd vimfiles
+
+git clone https://github.com/tpope/vim-pathogen.git
+
+mkdir bundle
+cd bundle
+git clone https://github.com/scrooloose/nerdtree
+git clone https://github.com/joshdick/onedark.vim
+git clone https://github.com/bling/vim-airline
+git clone https://github.com/sheerun/vim-polyglot
+git clone https://github.com/kien/ctrlp.vim
